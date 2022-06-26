3ad50fcfdc47dd5a29211c79503f92d567b0d6b8
Prateek Pai <paiprateek123@gmail.com>
2022-06-26 13:59:27 +0530

    Welcome to Binary Search Tree Problems

---
+++ BinarySearchTree_BST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree_BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to binary Search Tree");
+        }
+    }
+}
